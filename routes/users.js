00d8eac33071b5e19bb409d09ef1ff22c3648a12
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-08-18 14:02:10 +0530

    second

--- routes/users.js
+++ routes/users.js
@@ -32,9 +32,12 @@ router.post(
       return res.status(400).json({errors: errors.array()});
     }
   console.log(req.body);
-    const {email, password} = req.body;
+    const {name,email, password} = req.body;
+    console.log(email);
   try {
+    console.log(email)
       let user =await User.findOne({email});
+       console.log(user)
       
       if(user){
           return res.status(400).json({msg:"User already exists"})
@@ -42,6 +45,7 @@ router.post(
       user = new User({
           name,email,password
       })
+      console.log(user)
       const salt =await bcrypt.genSalt(10);
       console.log(salt);
       user.password =await bcrypt.hash(password,salt);
