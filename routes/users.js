79b52ac8b4a38c7dff6224aff3886366b5fca5d7
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-08-19 07:45:41 +0530

    first

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,15 @@ router.get('/',  (req, res) => {
   });
  
 });
+router.post("/:id",(req,res)=>{
 
+  const user= User.findOne({name :req.params.id})
+ 
+ 
+  res.send(user);
+    
+  
+ })
 
 
 // @route     POST api/auth
@@ -39,7 +47,7 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({errors: errors.array()});
     }
-  console.log(req.body);
+  
     const {name,email, password} = req.body;
     console.log(email);
   try {
@@ -58,16 +66,28 @@ router.post(
       console.log(salt);
       user.password =await bcrypt.hash(password,salt);
       await user.save();
-      res.send("User saved");
-  } 
-  catch(err)
-  {
-      
-      res.status(500).send("Serve Error");
-      console.log("error");
-  }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-   
-   })
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  },
+);
 
 module.exports = router;
