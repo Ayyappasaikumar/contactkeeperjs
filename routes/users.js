21c9e6001073388518c92b781cb0833830011336
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-08-20 19:57:58 +0530

    jwt

--- routes/users.js
+++ routes/users.js
@@ -2,65 +2,53 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
-const auth = require('../middleware/auth');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 const User = require('../models/User');
 
-// @route     GET api/auth
-// @desc      Get logged in user
-// @access    Private
-router.get('/',  (req, res) => {
-  User.find({name:"ayyappa"}, function(err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
- 
-});
-
-
-  
-
-
-
-// @route     POST api/auth
-// @desc      Auth user & get token
+// @route     POST api/users
+// @desc      Regiter a user
 // @access    Public
 router.post(
   '/',
-  [ check("name","please enter a name").not().isEmpty(),
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').not().isEmpty()
+    check(
+      'password',
+      'Please enter a password with 6 or more characters',
+    ).isLength({min: 6}),
   ],
-   async (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
-    console.log("hi");
     if (!errors.isEmpty()) {
       return res.status(400).json({errors: errors.array()});
     }
-  
-    const {name,email, password} = req.body;
-    console.log(email);
-  try {
-    console.log(email)
-      let user =await User.findOne({email});
-       console.log(user)
-      
-      if(user){
-          return res.status(400).json({msg:"User already exists"})
+
+    const {name, email, password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+
+      if (user) {
+        return res.status(400).json({msg: 'User already exists'});
       }
+
       user = new User({
-          name,email,password
-      })
-      console.log(user)
-      const salt =await bcrypt.genSalt(10);
-      console.log(salt);
-      user.password =await bcrypt.hash(password,salt);
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
       await user.save();
+
       const payload = {
         user: {
           id: user.id,
