aa7af789e11059caeb362d3ffeeb61180b66b277
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-08-21 11:22:03 +0530

    auth

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/",(req,res)=>{
 
 // app.get
 
-// app.use("/api/auth",require("./routes/auth"))
+app.use("/api/auth",require("./routes/auth"))
 app.use("/api/users",require("./routes/users"))
 app.use("/api/contacts",require("./routes/contacts"))
 const PORT=process.env.PORT || 6000;
